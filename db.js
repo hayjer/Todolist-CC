1b213ca846000b899e3c6baee9f5fc084b7bb4d7
Jeremy Hay <hayjer000@gmail.com>
2022-08-16 23:54:36 -0700

    Add db.js which contains database interaction methods, and implement todolist CRUD functions

---
+++ db.js
@@ -0,0 +1,54 @@
+//Database helpers sourced from www.sohamkamani.com/
+
+// import the `MongoClient` object from the library
+const { MongoClient, ObjectId } = require('mongodb')
+
+// define the connection string. If you're running your DB
+// on your laptop, this would most likely be it's address
+const connectionUrl = 'mongodb://localhost:27017'
+
+// Define the DB name. We will call ours `store`
+const dbName = 'todolist'
+
+// Create a singleton variable `db`
+let db
+
+// The init function returns a promise, which, once resolved,
+// assigns the mongodb connection to the `db` variable
+const init = () =>
+    MongoClient.connect(connectionUrl, { useNewUrlParser: true }).then((client) => {
+        db = client.db(dbName);
+    })
+
+// Take the item as an argument and insert it into the "tasks" collection
+const addTask = (task) => {
+    const collection = db.collection('tasks');
+    return collection.insertOne(task)
+}
+
+// get all items from the "tasks" collection
+const getTasks = () => {
+    const collection = db.collection('tasks');
+    return collection.find({}).toArray()
+}
+
+// get item with 'id' from "tasks" collection
+const getTaskByID = (id) => {
+    const collection = db.collection('tasks');
+    return collection.findOne({_id: ObjectId(id)})
+}
+
+// take the id and the replacement task, and update the name and description with the new fields
+const updateTask = (id, task) => {
+    const collection = db.collection('tasks');
+    return collection.replaceOne({ _id: ObjectId(id) }, task)
+}
+
+// delete the task with the given id
+const deleteTask = (id) => {
+    const collection = db.collection('tasks');
+    return collection.remove({_id: ObjectId(id)})
+}
+
+// export the required functions so that we can use them elsewhere
+module.exports = { init, addTask, getTasks, getTaskByID, updateTask, deleteTask }
--- todolist-api.js
+++ todolist-api.js
@@ -1,29 +1,92 @@
 const express = require('express');
+const crypto = require("crypto");
+const Joi = require('joi')
+const { init, addTask, getTasks, getTaskByID, updateTask, deleteTask } = require('./db.js');
+
 const app = express();
 const port = process.env.port || 3000;
 
+const taskSchema = Joi.object().keys({
+    name: Joi.string().length(32),
+    description: Joi.string()
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+    res.send("Todolist API");
+})
 
 app.get('/tasks', (req, res) => {
-    res.send('All todo-list tasks');
+    // Get tasks from DB
+    getTasks()
+        .then((tasks) => {
+            tasks = tasks.map((task) => ({
+                id: task._id,
+                name: task.name,
+                description: task.description
+            }));
+
+            res.json(tasks);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 app.get('/tasks/:id', (req, res) => {
-    res.send(`Task with id ${req.params.id}`);
+    getTaskByID(req.params.id)
+        .then((task) => {
+            let mappedTask = {
+                id: task._id,
+                name: task.name,
+                description: task.description
+            }
+
+            res.json(mappedTask);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 app.post('/tasks', (req, res) => {
-    res.send(`Created a new task with id FAKEID`);
+    const task = req.body;
+
+    const result = taskSchema.validate(task);
+    if(result.error) {
+        console.log(result.error);
+        res.sendStatus(400);
+        return
+    }
+
+    addTask(task)
+        .then(() => res.sendStatus(200))
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
 app.put('/tasks/:id', (req, res) => {
-    res.send(`Updated task with id ${req.params.id}`);
+    updateTask(req.params.id, req.body)
+        .then(() => res.sendStatus(200))
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
 app.delete('/tasks/:id', (req, res) => {
-    res.send(`Deleted task with id ${req.params.id}`);
+    deleteTask(req.params.id)
+        .then(() => res.sendStatus(200))
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+// Initialize DB connection and start the server
+init().then(() => {
+    app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+})
